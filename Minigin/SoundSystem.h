7c38212d0322a96b6283d0acda07845b9422103c
SebastianMaxG <leander.flossie@gmail.com>
2024-06-08 11:32:03 +0200

    link to signal (event)
    remove copy constructor and automatic link to the global event for sound

--- Minigin/SoundSystem.h
+++ Minigin/SoundSystem.h
@@ -3,22 +3,40 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
-
+#include "Signal.h"
 namespace lsmf
 {
+	namespace sound
+	{
+        inline std::jthread g_PlaySoundThread;
+        inline signal::Signal<const std::string&, float> PlaySoundSignal{g_PlaySoundThread};
+	}
+
+
 	class AudioClip;
 
 	class SoundSystem
     {
     public:
+        SoundSystem()
+        {
+        	m_PlaySoundConnection = sound::PlaySoundSignal.Connect(this, &SoundSystem::PlaySound);
+		}
         virtual ~SoundSystem() = default;
+
+        SoundSystem(const SoundSystem& other) = delete;
+        SoundSystem(SoundSystem&& other) = delete;
+        SoundSystem& operator=(const SoundSystem& other) = delete;
+        SoundSystem& operator=(SoundSystem&& other) = delete;
+
 
         virtual void PlaySound(const std::string& file, float volume) = 0;
         virtual void StopAllSounds() = 0;
         virtual void UnloadAll() = 0;
         virtual void Unload(const std::string& file) = 0;
         virtual void Load (const std::string& file) = 0;
-
+	protected:
+        signal::Connection<const std::string&, float>* m_PlaySoundConnection;
     };
 
     class SDLSoundSystem : public SoundSystem
