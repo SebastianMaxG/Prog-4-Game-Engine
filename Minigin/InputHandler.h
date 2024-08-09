bf8dc36321e0b4d940d6adf479833dbfed5ed5b5
SebastianMaxG <leander.flossie@gmail.com>
2024-08-09 14:46:07 +0200

    forgot to save file

--- Minigin/InputHandler.h
+++ Minigin/InputHandler.h
@@ -9,13 +9,19 @@ namespace lsmf
     class InputHandler : public Singleton<InputHandler>
     {
     public:
-        void BindCommand(const std::string& action, CommandPtr command);
-        void HandleInput(const std::string& action);
+        void BindCommand(const std::string& action, std::unique_ptr<Command> command);
+        bool HandleInput();
 
+        ~InputHandler() override;
     private:
         friend class Singleton<InputHandler>;
-        InputHandler() = default;
+        InputHandler();
 
         std::unordered_map<std::string, std::unique_ptr<Command>> m_CommandMap;
+
+
+        void OpenControllers();
+        void CloseControllers();
+        std::vector<SDL_GameController*> m_Controllers{};
     };
 }
